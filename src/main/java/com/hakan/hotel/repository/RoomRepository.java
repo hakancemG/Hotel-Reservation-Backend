25eaae4c45c62b95443388a8053051f4be248f37
Hakan Cem Gerçek <hakancg95@gmail.com>
2026-02-27 10:22:08 +0300

    Add RoomRepository with findByRoomNumber method.

---
+++ src/main/java/com/hakan/hotel/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.hakan.hotel.repository;
+
+import com.hakan.hotel.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    Optional<Room> findByRoomNumber(String roomNumber);
+
+}
