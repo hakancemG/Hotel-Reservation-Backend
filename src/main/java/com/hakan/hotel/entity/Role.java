2e7aef4988a41f93404b9e795820ba7d5b3cf2a1
Hakan Cem Gerçek <hakancg95@gmail.com>
2026-02-27 09:59:07 +0300

    Add Role entity with RoleType enum mapping.

---
+++ src/main/java/com/hakan/hotel/entity/Role.java
@@ -0,0 +1,34 @@
+package com.hakan.hotel.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private RoleType name;
+
+    // equals ve hashCode sadece id üzerinden, Lombok kullanabiliriz:
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role role)) return false;
+        return id != null && id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
